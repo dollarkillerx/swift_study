0edb7c19a620ae2e00c472739a9b403ccad31272
Никита Сергеевич Хрущёв <google@gmail.com>
2024-09-27 23:41:15 +0900

    demo

---
+++ FoodPickerPx/FoodPickerPx/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FoodPickerPx
+//
+//  Created by ouyo on 2024/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("dinner")
+                
+            Text("this is debug")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPickerPx/FoodPickerPx/FoodPickerPxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerPxApp.swift
+//  FoodPickerPx
+//
+//  Created by ouyo on 2024/09/27.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerPxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
